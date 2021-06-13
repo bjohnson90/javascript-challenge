cbbe3bb9ab1583b7ab38237ca6cd5e086137de64
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-06-13 11:16:32 -0500

    first stab at level 1

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,36 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+const content = d3.select("tbody")
+
+function genContents(data) {
+    content.html("")
+
+    data.forEach((contentRow) => {
+
+        const row = contents.append("tr");
+
+        Object.values(contentRow).firEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        })
+    })
+}
+
+function clickHandler() {
+    const dateInput = d3.select("#datatime").property("value");
+
+    let cleanedContent = content
+
+    if (dateInput){
+        cleanedContent = cleanedContent.filter(row => row.datetime === date)
+    }
+
+    genContents(cleanedContent)
+
+}
+
+d3.selectAll("#filter-btn").on("click", clickHandler)
+
+genContents(tableData)
+
